919d6306b480a13588b6f8bf50b10219cfebae9e
Michal Zajac <ZajoE38@gmail.com>
2019-02-12 16:38:46 +0100

    First commit

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Snake
+{
+    partial class frmSnake
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSnake));
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.imgList = new System.Windows.Forms.ImageList(this.components);
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(11, 11);
+            this.canvas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(420, 420);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            // 
+            // imgList
+            // 
+            this.imgList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgList.ImageStream")));
+            this.imgList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgList.Images.SetKeyName(0, "bonus1.png");
+            this.imgList.Images.SetKeyName(1, "bonus2.png");
+            this.imgList.Images.SetKeyName(2, "bonus3.png");
+            this.imgList.Images.SetKeyName(3, "bonus4.png");
+            this.imgList.Images.SetKeyName(4, "snake_body.png");
+            this.imgList.Images.SetKeyName(5, "snake_head.png");
+            this.imgList.Images.SetKeyName(6, "wall.png");
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 300;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // frmSnake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 446);
+            this.Controls.Add(this.canvas);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmSnake";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmSnake_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmSnake_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.ImageList imgList;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Snake/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class frmSnake : Form
+    {
+        #region Variables
+        enum GameItem { Free, Snake, Bonus };
+        enum Direction { Up, Down, Left, Right };
+        struct Coordinates
+        {
+            public int x;
+            public int y;
+        }
+
+        int snakeLength;
+        Direction headDirection;
+        GameItem[,] gameBoard;        
+        Coordinates[] snakeXY;
+        Graphics graphics;
+        Random random;
+        #endregion
+
+
+        public frmSnake()
+        {
+            InitializeComponent();
+            gameBoard = new GameItem[11, 11];
+            snakeXY = new Coordinates[100];
+            random = new Random();
+        }
+
+
+        private void frmSnake_Load(object sender, EventArgs e)
+        {
+            canvas.Image = new Bitmap(420, 420);
+            graphics = Graphics.FromImage(canvas.Image);
+            graphics.Clear(Color.White);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                graphics.DrawImage(imgList.Images[6], i * 35, 0);
+                graphics.DrawImage(imgList.Images[6], i * 35, 385);
+            }
+            for (int i = 0; i <= 11; i++)
+            {                
+                graphics.DrawImage(imgList.Images[6], 0, i * 35);
+                graphics.DrawImage(imgList.Images[6], 385, i * 35);
+            }
+
+            snakeXY[0].x = 5;
+            snakeXY[0].y = 5;
+            snakeXY[1].x = 5;
+            snakeXY[1].y = 6;
+            snakeXY[2].x = 5;
+            snakeXY[2].y = 7;
+
+            graphics.DrawImage(imgList.Images[5], 5 * 35, 5 * 35);
+            graphics.DrawImage(imgList.Images[4], 5 * 35, 6 * 35);
+            graphics.DrawImage(imgList.Images[4], 5 * 35, 7 * 35);
+
+            gameBoard[5, 5] = GameItem.Snake;
+            gameBoard[5, 6] = GameItem.Snake;
+            gameBoard[5, 7] = GameItem.Snake;
+
+            headDirection = Direction.Up;
+            snakeLength = 3;
+
+            for (int i = 0; i < 4; i++) Bonus();                      
+        }
+
+        private void Bonus()
+        {
+            int bonusType = random.Next(0, 4);
+            int x;
+            int y;
+
+            do
+            {
+                x = random.Next(1, 10);
+                y = random.Next(1, 10);
+            }
+            while (gameBoard[x,y] != GameItem.Free);
+
+            graphics.DrawImage(imgList.Images[bonusType], x * 35, y * 35);
+            gameBoard[x, y] = GameItem.Bonus;
+        }
+
+        private void GameOver(string str)
+        {
+            timer.Enabled = false;
+            MessageBox.Show("Game Over. " + str);
+        }
+
+        private void frmSnake_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    headDirection = Direction.Up;
+                    break;
+                case Keys.Down:
+                    headDirection = Direction.Down;
+                    break;
+                case Keys.Left:
+                    headDirection = Direction.Left;
+                    break;
+                case Keys.Right:
+                    headDirection = Direction.Right;
+                    break;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            graphics.FillRectangle(Brushes.White, snakeXY[snakeLength - 1].x * 35, snakeXY[snakeLength - 1].y * 35, 35, 35);
+            gameBoard[snakeXY[snakeLength - 1].x, snakeXY[snakeLength - 1].y] = GameItem.Free;
+
+            for (int i = snakeLength; i > 0; i--)
+            {
+                snakeXY[i].x = snakeXY[i - 1].x;
+                snakeXY[i].y = snakeXY[i - 1].y;
+            }
+
+            graphics.DrawImage(imgList.Images[4], snakeXY[0].x * 35, snakeXY[0].y * 35);
+
+            switch (headDirection)
+            {
+                case Direction.Up:
+                    snakeXY[0].y = snakeXY[0].y - 1;
+                    break;
+                case Direction.Down:
+                    snakeXY[0].y = snakeXY[0].y + 1;
+                    break;
+                case Direction.Left:
+                    snakeXY[0].x = snakeXY[0].x - 1;
+                    break;
+                case Direction.Right:
+                    snakeXY[0].x = snakeXY[0].x + 1;
+                    break;
+            }
+
+            if (snakeXY[0].x < 1 || snakeXY[0].x > 10 || snakeXY[0].y < 1 || snakeXY[0].y > 10)
+            {
+                GameOver("You hit the wall.");
+                return;
+            }
+
+            if (gameBoard[snakeXY[0].x, snakeXY[0].y] == GameItem.Snake)
+            {
+                GameOver("You hit your body.");
+                return;
+            }
+
+            if (gameBoard[snakeXY[0].x, snakeXY[0].y] == GameItem.Bonus)
+            {
+                graphics.DrawImage(imgList.Images[4], snakeXY[snakeLength].x * 35, snakeXY[snakeLength].y * 35);
+                gameBoard[snakeXY[snakeLength].x, snakeXY[snakeLength].y] = GameItem.Snake;
+                snakeLength++;
+                if (snakeLength < 96) Bonus();
+            }
+
+            graphics.DrawImage(imgList.Images[5], snakeXY[0].x * 35, snakeXY[0].y * 35);
+            gameBoard[snakeXY[0].x, snakeXY[0].y] = GameItem.Snake;
+
+            canvas.Refresh();
+        }
+    }
+}
